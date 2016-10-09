76503f1032721ac05ea8ecc1f7b92d2138abb118
xala3pa <alvaro.salazar@ticketbis.com>
2016-10-09 21:21:57 +0200

    add constructor to inyect behaviors

--- java/strategyPattern/strategyPattern/src/main/java/com/xala3pa/strategy/gateways/Gateway.java
+++ java/strategyPattern/strategyPattern/src/main/java/com/xala3pa/strategy/gateways/Gateway.java
@@ -5,6 +5,10 @@ import com.xala3pa.strategy.RuleBehavior;
 public abstract class Gateway {
     RuleBehavior ruleBehavior;
 
+    public Gateway(RuleBehavior ruleBehavior) {
+        this.ruleBehavior = ruleBehavior;
+    }
+
     public void setRuleBehavior(RuleBehavior ruleBehavior) {
         this.ruleBehavior = ruleBehavior;
     }
