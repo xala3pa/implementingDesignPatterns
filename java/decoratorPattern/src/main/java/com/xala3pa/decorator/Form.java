6081c732b4b63e42bda1551d525383895d9ced34
xala3pa <alvaro.salazar@ticketbis.com>
2016-10-23 09:59:23 +0200

    add Form simple decorator

---
+++ java/decoratorPattern/src/main/java/com/xala3pa/decorator/Form.java
@@ -0,0 +1,6 @@
+package com.xala3pa.decorator;
+
+public interface Form {
+    void draw();
+    String getDescription();
+}
---
+++ java/decoratorPattern/src/main/java/com/xala3pa/decorator/SimpleForm.java
@@ -0,0 +1,13 @@
+package com.xala3pa.decorator;
+
+public class SimpleForm implements Form {
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public String getDescription() {
+        return "Simple web form...";
+    }
+}
