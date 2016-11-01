80f8075dde7f962bd8dcdcd7c20ca2e4d548b728
xala3pa <alvaro.salazar@ticketbis.com>
2016-11-01 11:10:43 +0100

    Use data driven to  test academy options

--- java/factoryMethodPattern/src/test/groovy/com/xala3pa/factoryMethod/BattleFieldSpec.groovy
+++ java/factoryMethodPattern/src/test/groovy/com/xala3pa/factoryMethod/BattleFieldSpec.groovy
@@ -7,55 +7,27 @@ import com.xala3pa.factoryMethod.army.NYMarine
 import com.xala3pa.factoryMethod.army.NYSniper
 import com.xala3pa.factoryMethod.army.SFMarine
 import com.xala3pa.factoryMethod.army.SFSniper
-import com.xala3pa.factoryMethod.army.Soldier
+import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class BattleFieldSpec extends Specification {
 
-    Academy academy;
-    Optional<Soldier> soldier;
+    @Shared
+    Academy nyAcademy = new NYAcademy()
+    @Shared
+    Academy sfAcademy = new SFAcademy()
 
-    def "Should  received a trained NY marine soldier from the academy"() {
-        given: "NY Academy"
-        academy = new NYAcademy();
+    @Unroll
+    def "Should  received a trained #soldierType soldier from the #academy"() {
+        expect:
+        academy.trainSoldier(soldierType).get().class == soldierClass
 
-        when: "we for training a soldier"
-        soldier = academy.trainSoldier("NY Marine");
-
-        then: "we receive a trained NY marine soldier"
-        soldier.get().class == NYMarine
-    }
-
-    def "Should  received a trained NY Sniper soldier from the academy"() {
-        given: "NY Academy"
-        academy = new NYAcademy();
-
-        when: "we for training a soldier"
-        soldier = academy.trainSoldier("NY Sniper");
-
-        then: "we receive a trained NY Sniper soldier"
-        soldier.get().class == NYSniper
-    }
-
-    def "Should  received a trained SF marine soldier from the academy"() {
-        given: "SF Academy"
-        academy = new SFAcademy();
-
-        when: "we for training a soldier"
-        soldier = academy.trainSoldier("SF Marine");
-
-        then: "we receive a trained SF marine soldier"
-        soldier.get().class == SFMarine
-    }
-
-    def "Should  received a trained SF Sniper soldier from the academy"() {
-        given: "SF Academy"
-        academy = new SFAcademy();
-
-        when: "we for training a soldier"
-        soldier = academy.trainSoldier("SF Sniper");
-
-        then: "we receive a trained SF Sniper soldier"
-        soldier.get().class == SFSniper
+        where:
+        academy   | soldierType || soldierClass
+        nyAcademy | "NY Marine" || NYMarine
+        nyAcademy | "NY Sniper" || NYSniper
+        sfAcademy | "SF Marine" || SFMarine
+        sfAcademy | "SF Sniper" || SFSniper
     }
 }
