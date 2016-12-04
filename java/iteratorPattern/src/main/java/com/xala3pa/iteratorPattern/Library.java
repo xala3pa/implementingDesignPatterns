4621831cebe0286054f665f7ac8e56db3ee44e85
xala3pa <salazar3pa@gmail.com>
2016-12-04 20:22:20 +0100

    Iterate over all iterables to get all books

--- java/iteratorPattern/src/main/java/com/xala3pa/iteratorPattern/Library.java
+++ java/iteratorPattern/src/main/java/com/xala3pa/iteratorPattern/Library.java
@@ -1,19 +1,22 @@
 package com.xala3pa.iteratorPattern;
 
+import java.util.*;
+
 public class Library {
     public static void main(String[] args) {
-        ITBookRepository itBookRepository = new ITBookRepository();
-        NovelRepository novelRepository = new NovelRepository();
 
-        for(Iterator iter = itBookRepository.getIterator(); iter.hasNext();){
-            String book = (String)iter.next();
-            System.out.println("IT Book : " + book);
+        HashMap<String, Iterable> allBooks = new HashMap<String, Iterable>();
+        allBooks.put("IT books",  new ITBookRepository());
+        allBooks.put("Novels",  new ITBookRepository());
+
+        java.util.Iterator booksIterator = allBooks.entrySet().iterator();
+        while (booksIterator.hasNext()) {
+            Map.Entry pair = (Map.Entry)booksIterator.next();
+            Iterable iterable = (Iterable) pair.getValue();
+            for(Iterator iter = iterable.getIterator(); iter.hasNext();){
+                String book = (String)iter.next();
+                System.out.println(pair.getKey() + " : " + book);
+            }
         }
-
-        for(Iterator iter = novelRepository.getIterator(); iter.hasNext();){
-            String book = (String)iter.next();
-            System.out.println("Novel : " + book);
-        }
-
     }
 }
